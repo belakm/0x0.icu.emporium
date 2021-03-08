e615d62d5f569dce4f5c63a6aa16018cc489f000
Mitja Belak <mico@0x0.icu>
2021-03-08 15:44:16 +0100

    fix with import

--- graphql/src/server.js
+++ graphql/src/server.js
@@ -1,6 +1,6 @@
 import * as http from 'http';
 import { postgraphile, makePluginHook } from 'postgraphile'
-import { makeAllowedOriginTweak } from './plugins/cors'
+import makeAllowedOriginTweak from './plugins/cors'
 
 const pluginHook = makePluginHook([
   makeAllowedOriginTweak('localhost'),
