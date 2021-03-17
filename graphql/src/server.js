60e70335e98255eac2c9675b0df41ca41f87d6bc
Mitja Belak <mico@0x0.icu>
2021-03-17 23:59:18 +0100

    Added express as server

--- graphql/src/server.js
+++ graphql/src/server.js
@@ -1,19 +1,45 @@
-import * as http from 'http';
+import helmet from 'helmet'
+import express from 'express'
+import compression from 'compression';
+import bodyParser from 'body-parser'
 import { postgraphile, makePluginHook } from 'postgraphile'
 import makeAllowedOriginTweak from "./plugins/cors.js"
 
+// allow localhost development
 const pluginHook = makePluginHook([
   makeAllowedOriginTweak('http://localhost:1337'),
 ]);
 
-http
-  .createServer(
-    postgraphile(process.env.DATABASE_URL, '_0x0', {
-      watchPg: true,
-      graphiql: true,
-      enhanceGraphiql: true,
-      retryOnInitFail: true,
-      pluginHook
-    })
-  )
-  .listen(process.env.PORT);
+const app = express()
+
+// compression of responses
+app.use(compression())
+
+// headers protection
+app.use(helmet())
+
+// body-parser
+const { json, urlencoded, text } = bodyParser
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(text({ type: 'application/graphql' }));
+
+// postgraphile
+app.use(postgraphile(process.env.DATABASE_URL, '_0x0', {
+  watchPg: true,
+  graphiql: true,
+  enhanceGraphiql: true,
+  retryOnInitFail: true,
+  pluginHook
+}))
+
+// start express server
+const server = app.listen(process.env.PORT, () => {
+  const address = server.address();
+  if (typeof address !== 'string') {
+    const href = `http://localhost:${address.port}${options.graphiqlRoute || '/graphiql'}`;
+    console.log(`PostGraphiQL available at ${href} 🚀`);
+  } else {
+    console.log(`PostGraphile listening on ${address} 🚀`);
+  }
+});
